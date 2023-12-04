a44bab253a423b9fab24a109113e3b2c745a34ba
Nmartins6 <nicolas.edm7@gmail.com>
2023-12-04 14:56:39 -0300

    feat: Refactor image cropping with dynamic sizing

--- scan_cleaner.py
+++ scan_cleaner.py
@@ -1,15 +1,24 @@
 import cv2
 
-image = cv2.imread('image_path.jpg')
+image = cv2.imread('input/test_image.jpg')
 
-x, y, width, height = 0, 0, 1150, 1700
+height, width, _ = image.shape
 
-cropped_image = image[y:y+height, x:x+width]
+percentage_width = 0.5
+percentage_height = 0.5
 
-cv2.rectangle(image, (x, y), (x + width, y + height), (0, 255, 0), 2)
+x = 0
+y = 0
+crop_width = int(width * percentage_width) 
+crop_height = int(height * percentage_height)
 
-cv2.imshow('Original Image', image)
+cv2.rectangle(image, (x, y), (x + crop_width, y + crop_height), (0, 255, 0), 2)
+
+cv2.imshow('Image with Contour', image)
+cv2.waitKey(0)
+
+cropped_image = image[y:y+crop_height, x:x+crop_width]
+
 cv2.imshow('Cropped Image', cropped_image)
-
 cv2.waitKey(0)
 cv2.destroyAllWindows()
