90897c2dc03bf751389ccf2d9ad49365541b7c95
Nmartins6 <nicolas.edm7@gmail.com>
2023-12-04 15:30:29 -0300

    feat: Enhance contour detection for targeted image cropping

--- scan_cleaner.py
+++ scan_cleaner.py
@@ -1,24 +1,22 @@
 import cv2
 
 image = cv2.imread('input/test_image.jpg')
+original = image.copy()
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+thresh = cv2.threshold(blurred, 230, 255, cv2.THRESH_BINARY_INV)[1]
 
-height, width, _ = image.shape
+contours = cv2. findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours = contours[0] if len(contours) == 2 else contours [1]
 
-percentage_width = 0.5
-percentage_height = 0.5
+image_number = 0
+for c in contours:
+    x,y,w,h = cv2.boundingRect(c)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
+    ROI = original[y:y+h, x: x+w]
+    cv2.imwrite("photo_{}.jpg". format(image_number), ROI)
+    image_number += 1
 
-x = 0
-y = 0
-crop_width = int(width * percentage_width) 
-crop_height = int(height * percentage_height)
-
-cv2.rectangle(image, (x, y), (x + crop_width, y + crop_height), (0, 255, 0), 2)
-
-cv2.imshow('Image with Contour', image)
+cv2.imshow('thresh', thresh)
+# cv2.imshow('image', image)
 cv2.waitKey(0)
-
-cropped_image = image[y:y+crop_height, x:x+crop_width]
-
-cv2.imshow('Cropped Image', cropped_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
